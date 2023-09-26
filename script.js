9a28786c377564baf96a69f2109a2681827cc309
ivanwijdan16 <ivanok152@gmail.com>
2023-09-26 20:39:28 +0700

    create function btn DEL

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 document.querySelector("#clear").addEventListener("click", () => {
   document.querySelector("#display").value = " ";
 });
+
+//delete
+const deleteNum = () => {
+  const num = document.querySelector("#display").value.slice(0, -1);
+  document.querySelector("#display").value = num;
+};
+
+//
