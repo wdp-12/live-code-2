8288b16e24ce245a9856800d06bdc15e915c40b5
Arizidane <samionohari@gmail.com>
2023-09-26 21:11:22 +0700

    add: Create operand & sum functional

--- script.js
+++ script.js
@@ -19,7 +19,54 @@ createButton('operator', '*', '#operator')
 createButton('operator', '/', '#operator')
 
 // create button clear
-createButton('clear', 'clear', '#operator')
+createButton('clear', 'clear', '#input')
 
-// create button operator
-createButton('hitung', '=', '#operator')
+// create button hitung
+createButton('hitung', '=', '#input')
+
+// display number dg event
+let data = ''
+let btnElement = document.getElementsByClassName("number")
+for (let i = 0; i < btnElement.length; i++) {
+    btnElement[i].addEventListener('click', () => {
+        document.getElementById("result").innerHTML = data += btnElement[i].value;
+    })
+}
+
+// display operator dg event
+let opElement = document.getElementsByClassName("operator")
+for (let i = 0; i < opElement.length; i++) {
+    opElement[i].addEventListener('click', () => {
+        document.getElementById("result").innerHTML = data += opElement[i].value
+    })
+}
+
+// fungsi button clear dg event
+document.querySelector(".clear").addEventListener('click', () => {
+    data = ''
+    document.getElementById("result").innerHTML = ''
+})
+
+// fungsi hitung number dg event
+document.querySelector(".hitung").addEventListener('click', () => {
+    // split char operator
+    let tambah = data.split('+')
+    let kurang = data.split('-')
+    let kali = data.split('*')
+    let bagi = data.split('/')
+
+    // percabangan berdasarkan operator
+    if (data.includes('+')) {
+        data = Number(tambah[0])+Number(tambah[1])
+        document.getElementById("result").innerHTML = data
+    } else if (data.includes('-')) {
+        data = Number(kurang[0])-Number(kurang[1])
+        document.getElementById("result").innerHTML = data
+    } else if (data.includes('*')) {
+        data = Number(kali[0])*Number(kali[1])
+        document.getElementById("result").innerHTML = data
+    } else if (data.includes('/')) {
+        data = Number(bagi[0])/Number(bagi[1])
+        document.getElementById("result").innerHTML = data
+    }
+})
