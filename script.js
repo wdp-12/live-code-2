8da0f95ec823b1001041c0080de04d9e8b0e5cfd
Rozan-Latukau <iyanlatu@gmail.com>
2023-09-26 21:19:58 +0700

    membuat js

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-function insert(num) {
+function nilai(num) {
 
-
-    let a = document.form.box;
-
-    a.value += num;
 }
-
-// function equal() {
-//     var a = document.form.textview;
-//     var exp = a.value;
-
-//     if (exp) {
-//         a.value = eval(exp);
-//     }
-// }
-
-// function clean() {
-//     document.form.textview.value = '';
-// }
-
-// function back() {
-//     var exp = document.form.textview.value;
-
-//     document.form.textview.value = exp.substring(0, exp.length - 1);
-// }
