d1dfb0906dec5129bceb7d3328f1a013fd88cac8
yabsiraditya <yabsir662@gmail.com>
2023-09-26 21:15:25 +0700

    update javascript

---
+++ src/script.js
@@ -0,0 +1,37 @@
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+  };
+
+function updateDisplay() {
+    const display = document.querySelector('.input-screen');
+    display.value = calculator.displayValue;
+}
+
+const keys = document.querySelector('.input-screen');
+keys.addEventListener('click', (event) => {
+  const { target } = event;
+
+  if (!target.matches('button')) {
+    return;
+  }
+
+  if (target.classList.contains('operator')) {
+    console.log('operator', target.value);
+    return;
+  }
+
+  if (target.classList.contains('decimal')) {
+    console.log('decimal', target.value);
+    return;
+  }
+
+  if (target.classList.contains('all-clear')) {
+    console.log('clear', target.value);
+    return;
+  }
+
+  console.log('digit', target.value);
+});
