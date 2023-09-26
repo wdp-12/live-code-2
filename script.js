c2d1e60f22b3bbb925542473c8b13326934d116e
azimatu18 <azimatu447@gmail.com>
2023-09-26 21:14:41 +0700

    membuat function appendToDisplay(value) dan function calculate() tapi belum selesai

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 function clearDisplay() {
     document.getElementById('display').value = '';
 }
+
+function appendToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculate() {
+    if ( value){
+
+    }
+}
